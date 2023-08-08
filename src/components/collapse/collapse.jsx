5b0681faba9a01df16e174609d302479f3cc0407
Maxime J <125677181+maxxxiiime@users.noreply.github.com>
2023-08-08 15:07:27 +0200

    Test Collapse About NO work

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import IconCollapse from '../../assets/icon-collapse.svg'
 
-export default function collapse() {
+export default function Collapse({ title, content }) {
+
+  const [hide, show] = useState(false);
+
+  const toggleCollapse = (event) => {
+    console.log("je collapse");
+    show(!hide);
+};
+
   return (
-    <div>collapse</div>
+    <div className='collapse-bar'>
+      <div className='collapse-bar_title'>
+        <h2>{title} </h2>
+        <div className='to-toggle-collapse'>
+          <img src={ IconCollapse }
+          alt='icon collapse' 
+          className='icon-collapse'
+          onClick={toggleCollapse} 
+          
+          /> 
+        </div>
+      </div>
+      <div className='collapse-bar_text'>
+        <p> {content}</p>
+      </div>
+    </div>
   )
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,14 +3,21 @@ import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import Banner from '../components/banner/Banner';
 import slide2 from '../assets/slide2.jpg'
+import Collapse from '../components/collapse/Collapse';
 
 export default function About() {
   return (
     <div>
-<Header />
-<Banner image={slide2} />
-      <h1>à propos</h1>
-      <Footer />
+    <Header />
+
+    <Banner image={slide2} />
+          <h1>à propos</h1>
+
+          <div className='about_collapse_content'>
+    <Collapse  title=" testte"
+              content="on verra plus tard bnalablaa on verra plus tard bnalablaa on verra plus tard bnalablaaon verra plus tard bnalablaa" />
+        </div>
+    <Footer />
     </div>
   )   
 }
