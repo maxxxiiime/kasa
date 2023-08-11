5dddd8c11273269aa94b164b91be45f0ea88bc02
Maxime J <125677181+maxxxiiime@users.noreply.github.com>
2023-08-11 16:36:28 +0200

    Slider + logement

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -12,12 +12,33 @@ export default function Slider({image}) {
       updateIndex(indexSlider === image.length -1 ? 0 : indexSlider +1)
       console.log(indexSlider);
     }
+    const backSlide = () => {
+      updateIndex(indexSlider === 0 ? image.length - 1 : indexSlider -1)
+      console.log(indexSlider);
+    }
+    //Numero de l'image dans le slider
+    // const indexSliderNum = indexSlider; 
+    const allImages = image.length; 
 
   return (
     <div className='slider'>
         <img src={image[indexSlider]} alt='slider_img' className='slider_img' />
      
-    
+        <div className='nav-slide'>
+              <img src={nextArrow} alt='next slide' className="next-slide"
+                onClick={nextSlide} 
+              /> 
+            
+              <img src={backArrow} alt='back slide' className="back-slide"
+                onClick={backSlide} 
+              /> 
+        </div>
+        <div className='index-slider'>
+          <p>
+            {indexSlider}/{allImages}
+          </p>
+        </div>
+
     </div>
   )
 }
--- src/pages/FicheLogement.jsx
+++ src/pages/FicheLogement.jsx
@@ -3,7 +3,9 @@ import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import CardsDatas from '../datas/logements.json';
 import Slider from '../components/slider/Slider';
-import slide1 from '../assets/slide1.jpg'
+import Collapse from '../components/collapse/Collapse';
+import './styles/logement.scss'
+
 export default function FicheLogement( ) {
   const queryParams = new URLSearchParams(window.location.search)
 
@@ -13,12 +15,31 @@ export default function FicheLogement( ) {
  })[0]
 console.log(logement)
 console.log(idLog)
-
+// const pictures = logement ? logement.pictures : [];
 
   return (
     <div>
          <Header />
-          <Slider />
+          <Slider image={logement.pictures}/>
+          <div className='all-description'>
+            <div className='_text-left'>
+            <h2>{logement.title}</h2>
+            <p>{logement.location}</p>   
+            </div>
+              <div className='tags'>
+                {logement.tags}
+              </div>
+                    <div className='host'>
+                        <div className="host-name">{logement.host.name}
+                        </div>
+                        <img src = {logement.host.picture} alt = {logement.host.name} />
+                    </div>
+                <div className='stars'>{logement.rating}</div>
+                <div className='collapse-logement'>
+                <Collapse  title=" Description" content= {logement.description} className='description-collapse' />
+                <Collapse  title=" Équipements" content= {logement.equipments}  />
+                </div>
+          </div>
          <Footer />
        </div>
   )
